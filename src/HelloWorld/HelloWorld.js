64f6bbbc968dfdd89b89a2fe5d58f0ad200c4f82
BoondockTaints <wsollers@gmail.com>
2023-11-12 13:39:26 -0500

    Simple Hello World
    Simple Hello World

---
+++ src/HelloWorld/HelloWorld.js
@@ -0,0 +1,5 @@
+/*
+ * Just a simple Hello World
+ */
+var message = 'Hello World';
+console.log(message);
---
+++ src/HelloWorld/HelloWorld.ts
@@ -0,0 +1,8 @@
+/*
+ * Just a simple Hello World
+ */
+ let message: string = 'Hello World';
+console.log(message);
+
+
+
